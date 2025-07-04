a944f2dca0023bdb4ebafe19f9841795a86205ee
mollygit001 <arshadzamaaz+github@gmail.com>
2025-07-04 23:21:54 +0530

    feat: add configuration for server endpoint and Google client ID

---
+++ src/config/config.js
@@ -0,0 +1,4 @@
+const serverEndpoint = import.meta.env.VITE_SERVER_ENDPOINT;
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENTID;
+
+export { serverEndpoint, googleClientId };
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
-import store  from './app/store.js'
 import { Provider } from 'react-redux'
+import './index.css'
+import App from './App.jsx'
+import store  from './app/store.js'
+import { googleClientId } from './config/config.js'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <GoogleOAuthProvider clientId="834480753840-nnri89cc1ivejub3pf728pe4379nvrps.apps.googleusercontent.com">
+      <GoogleOAuthProvider clientId={googleClientId}>
         <Provider store={store}>
         <App />
         </Provider>
